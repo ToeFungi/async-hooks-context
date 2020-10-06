4e8aa570f283df332defc4148b30f9076f1fe274
Alex <6685713+ToeFungi@users.noreply.github.com>
2020-10-06 13:38:08 +0200

    feat(context): Update the context mechanism to not wrap (#6)

--- test/unit/AsyncHooksContext.spec.ts
+++ test/unit/AsyncHooksContext.spec.ts
@@ -1,38 +1,62 @@
-import { AsyncHooksContexts } from '../../src/AsyncHooksContext'
+import * as uuid from 'uuid'
+
+import { createSandbox } from 'sinon'
+
 import { Bar, Foo } from '../mocks/Foo'
+import { createRequestContext } from '../../src/AsyncHooksContext'
 
 describe('AsyncHooksContext', () => {
+  const sandbox = createSandbox()
+
   const contextId = 'some-context-id'
 
   it('returns the correct context ID when the class is wrapped immediately', () => {
     const foo = new Foo()
 
-    const context = AsyncHooksContexts.initContext((context: any) => {
-      context.id = contextId
-      return foo.bar()
-    })
+    createRequestContext({}, contextId)
 
-    context.should.have.property('id')
-    context.id.should.deep.equal(contextId)
+    const { correlationId } = foo.bar()
+
+    correlationId.should.deep.equal(contextId)
   })
 
   it('returns the correct context ID when the class is injected into another class', () => {
     const foo = new Foo()
     const bar = new Bar(foo)
 
-    const context = AsyncHooksContexts.initContext((context: any) => {
-      context.id = contextId
-      return bar.foobar()
-    })
+    createRequestContext({}, contextId)
+    const { correlationId } = bar.foobar()
 
-    context.should.have.property('id')
-    context.id.should.deep.equal(contextId)
+    correlationId.should.deep.equal(contextId)
   })
 
   it('returns an empty object when the context cannot be found by execution ID', () => {
+    const generatedUuid = 'xxxx-xxxx-xxxx-xxxx'
+
     const foo = new Foo()
 
+    const v4Stub = sandbox.stub(uuid, 'v4')
+    v4Stub.returns(generatedUuid)
+
+    createRequestContext()
+    const { correlationId } = foo.bar()
+    correlationId.should.deep.equal(generatedUuid)
+  })
+
+  it('returns the data that was stored for the request context', () => {
+    const data = {
+      foo: 'bar'
+    }
+    const expectedResponse = {
+      data,
+      correlationId: contextId
+    }
+
+    const foo = new Foo()
+
+    createRequestContext(data, contextId)
+
     const context = foo.bar()
-    context.should.deep.equal({})
+    context.should.deep.equal(expectedResponse)
   })
 })
